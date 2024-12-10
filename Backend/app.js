1f0e97bc0434069698feea98147fba18d5aa4e58
Muhammad Arslan <charslan00713@gmail.com>
2024-12-10 18:24:44 +0500

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -3,14 +3,17 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const connectToDb = require('./db/db')
 const userRoutes = require('./routes/user.routes')
+
 
 connectToDb()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send("hello")
